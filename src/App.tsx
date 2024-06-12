526cf6b800e18fac2f8070d86933e94b2f207e2a
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2024-06-12 11:38:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-auto py-8 px-4 bg-slate-800">
+        <h1 className="text-3xl mb-12 text-white text-center">
+          Appear
+        </h1>
+        <div className='grid grid-cols-8 gap-4'>
+          <div className='w-full h-80 col-span-8 rounded-md bg-teal-500'></div>
+          <div className='h-52 animate-appear col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-slate-500 animate-appear'></div>
+          <div className='h-52 col-span-4 rounded-md bg-red-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-green-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-blue-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-violet-500 animate-appear'></div>
+          <div className='h-52 col-span-2 rounded-md bg-orange-500 animate-appear'></div>
+          <div className='h-52 col-span-8 rounded-md bg-purple-500 animate-appear'></div>
+          <div className='w-full h-80 col-span-8 rounded-md bg-teal-500'></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
